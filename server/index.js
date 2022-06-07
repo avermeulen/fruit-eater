8f5b299052004bc4104e617f4668701a06b004a7
avermeulen <andre.vermeulen@gmail.com>
2022-06-07 18:13:09 +0200

    connected back-end and frontend

--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
 // API code here.
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+const fruits = {
+	apple: 5,
+	pear : 7,
+	banana : 23
+};
+
+app.post('/api/fruits/:fruit', function(req, res){
+	const {fruit} = req.params;
+	if (!fruits[fruit]){
+		fruits[fruit] = 0
+	}
+	fruits[fruit]++
+	res.json({})
+});
+
+app.get('/api/fruits', function(req, res){
+	res.json({fruits})
+})
+
+const PORT = process.env.PORT || 3011;
+
+// console.log(PORT);
+app.listen(PORT, () => console.log(`App started on ${PORT}`))
