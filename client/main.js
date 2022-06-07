953809828af26c47e2b68a6bd507baea3dd3b315
avermeulen <andre.vermeulen@gmail.com>
2022-06-07 18:14:33 +0200

    connected back-end and frontend

--- client/main.js
+++ client/main.js
@@ -1,18 +1,37 @@
 import './style.css'
 
 import Alpine from 'alpinejs'
+import axios from 'axios';
  
 window.Alpine = Alpine
 
+const URL_BASE = import.meta.env.VITE_SERVER_URL;
+
 Alpine.data('fruitEater', function() {
   return {
+    init() {
+
+      const url = `${URL_BASE}/api/fruits`
+
+      axios
+        .get(url)
+        .then((result) => {
+          console.log(result.data)
+          this.fruits = {...result.data.fruits}
+        })
+    },
+
     fruits : {
       apple: 0,
       pear : 0,
       banana : 0
     },
     eat(fruit) {
-      this.fruits[fruit]++;
+
+      const url = `${URL_BASE}/api/fruits/${fruit}`
+      axios
+        .post(url)
+        .then(() => this.fruits[fruit]++)
     }
   }
 });
